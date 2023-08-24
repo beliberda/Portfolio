3a7e7c8c298767af529267a7cee5808a61db3b51
beliberda <anton.nurgaliev@mail.ru>
2023-08-24 13:31:29 +1000

    dede

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,3 +42,52 @@ projectMass.map((project)=>{
     <a href="${project.link}"><button>look it up</button></a>
   </article>`)
 })
+
+let techList = document.querySelector(".technology-list")
+
+let techMass = [
+    {
+        title: "Html",
+        lvl: 80
+    },
+    {
+        title: "CSS",
+        lvl: 80
+    },
+    {
+        title: "Js",
+        lvl: 60
+    },
+    {
+        title: "React",
+        lvl: 50
+    },
+    {
+        title: "NextJs",
+        lvl: 40
+    },
+    {
+        title: "VueJs",
+        lvl: 30
+    },
+    {
+        title: "Ionic",
+        lvl: 20
+    },
+    {
+        title: "TypeScript",
+        lvl: 20
+    }
+]
+
+techMass.forEach((item)=>{
+    techList.insertAdjacentHTML("beforeend", `<li class="technology-item">
+            <div class="technology-item__top">
+              <h2 class="technology-item__title">${item.title}</h2>
+              <h3 class="technology-item__level">${item.lvl < 40 ? "Beginner": "Advanced"}</h3>
+            </div>
+            <div class="technology-item__bottom">
+              <div style="width:${item.lvl}%" class="technology-item__top-line"></div>
+            </div>
+          </li>`)
+})
