947135e5676e4bcc42bfdd657388b6570dadc88a
beliberda <anton.nurgaliev@mail.ru>
2023-09-01 11:52:23 +1000

    dede

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,5 @@
 // массив объектов, где один объект - один проект
-projectMass = [
-    {
-        // У каждого проекта есть изображение, заголовок, текст и ссылка
-        // ВНИМАНИЕ! Путь до изображения должен быть относительно index.html, а не index.js
-        image: './assets/images/project.png',
-        title: 'Parallax3D',
-        text:'Site with 3D Effect, rain and relax music',
-        link:'https://beliberda.github.io/Parallax3D/'
-    },
-    {
-        image: './assets/images/project.png',
-        title: 'Бункер',
-        text:'Генератор персонажей для настольной игры Бункер',
-        link:'https://beliberda.github.io/Parallax3D/'
-    },
-    {
-        image: './assets/images/project.png',
-        title: 'Мафия',
-        text:'Сайт для распределения ролей в игре "Мафия"',
-        link:'https://beliberda.github.io/Parallax3D/'
-    },
-    {
-        image: './assets/images/project.png',
-        title: 'Fukuro',
-        text:'Сайт продажи авто из Японии',
-        link:'https://beliberda.github.io/Parallax3D/'
-    },
-]
+import projectMass from './projects.js'
 // Получаем элемент с id "project-list"
 let list = document.getElementById("project-list")
 // Проходим по массиву с проектами с помощью метода map
---
+++ assets/js/projects.js
@@ -0,0 +1,29 @@
+const projectMass = [
+    {
+        // У каждого проекта есть изображение, заголовок, текст и ссылка
+        // ВНИМАНИЕ! Путь до изображения должен быть относительно index.html, а не index.js
+        image: './assets/images/project.png',
+        title: 'Parallax3D',
+        text:'Site with 3D Effect, rain and relax music',
+        link:'https://beliberda.github.io/Parallax3D/'
+    },
+    {
+        image: './assets/images/project.png',
+        title: 'Бункер',
+        text:'Генератор персонажей для настольной игры Бункер',
+        link:'https://beliberda.github.io/Parallax3D/'
+    },
+    {
+        image: './assets/images/project.png',
+        title: 'Мафия',
+        text:'Сайт для распределения ролей в игре "Мафия"',
+        link:'https://beliberda.github.io/Parallax3D/'
+    },
+    {
+        image: './assets/images/project.png',
+        title: 'Авто',
+        text:'Сайт продажи авто из Японии',
+        link:'https://beliberda.github.io/Parallax3D/'
+    },
+]
+export default projectMass
